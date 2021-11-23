96fee3304c7ba65f4799d58f4404af4890305d02
Chinapat <skybluezone4@gmail.com>
2021-11-23 17:13:56 +0700

    First commit

---
+++ environment_setup/install-requirements.sh
@@ -0,0 +1,7 @@
+python --version
+#pip install --upgrade azure-cli
+pip install azure-cli==2.30.0
+pip install --upgrade azureml-sdk[cli]
+pip install pytest
+pip install pytest-cov
+#pip install -r requirements.txt
